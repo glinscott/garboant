d010ed87f5c5d1feddf3fbef1c3369bdb391f64e
Gary Linscott <glinscott@gmail.com>
2011-11-06 23:53:33 -0500

    Track ant state from turn to turn

--- MyBot.go
+++ MyBot.go
@@ -7,13 +7,30 @@ import (
 	"os"
 )
 
+type AntState int8
+const (
+	STATE_IDLE = iota
+	STATE_HUNT_FOOD
+	STATE_EXPLORE
+)
+
+type Ant struct {
+	loc						Location
+	target				Location
+	closestFood 	Location
+	state 				AntState
+	seenThisTurn 	bool
+}
+
 type GarboAnt struct {
 	visible map[Location]float64
+	ants map[Location]*Ant
 }
 
 func NewBot(s *State) Bot {
 	me := &GarboAnt{
 		visible: make(map[Location]float64),
+		ants: make(map[Location]*Ant),
 	}
 	return me
 }
@@ -64,18 +81,34 @@ func (me *GarboAnt) SearchMap(s *State, source Location, isFinalState func(curre
 
 //DoTurn is where you should do your bot's actual work.
 func (me *GarboAnt) DoTurn(s *State) os.Error {
-	type AntState struct {
-		loc Location
-		closestFood Location
-		huntingFood bool
+	// Mark all ants as not seen so far
+	for _, ant := range me.ants {
+		ant.seenThisTurn = false
 	}
-	
-	myAnts := []*AntState{}
+
+	// Check to see which ants are alive and in the place we thought they should be
 	for loc, ant := range s.Map.Ants {
 		if ant != MY_ANT {
 			continue
 		}
-		myAnts = append(myAnts, &AntState{ loc: loc, huntingFood: false,})
+	 	_, exists := me.ants[loc]
+		if !exists {
+			me.ants[loc] = &Ant{ state: STATE_EXPLORE, }
+			me.ants[loc].loc = loc
+		} else {
+			if me.ants[loc].loc != loc {
+				log.Println("Ant state corrupted")
+			}
+		}
+		me.ants[loc].seenThisTurn = true
+	}
+	
+	for loc, ant := range me.ants {
+		// Remove killed ants from state
+		if !ant.seenThisTurn {
+			me.ants[loc] = nil, false
+			log.Println("Ant killed at ", loc)
+		}
 	}
 	
 	// Reduce the visibility
@@ -86,39 +119,49 @@ func (me *GarboAnt) DoTurn(s *State) os.Error {
 		}
 	}
 
-	// Mark the spots we can see as visible, check for food
-	for _, ant := range myAnts {
-		closest := 9999999
-		fRow, fCol := s.Map.FromLocation(ant.loc)
-		s.Map.DoInRad(ant.loc, s.ViewRadius2, func(row, col int) {
-			loc := s.Map.FromRowCol(row, col)
-			me.visible[loc] = 1.0
-			
-			if s.Map.Food[loc] {
-				distance := (fRow-row)*(fRow-row)+(fCol-col)*(fCol-col);
-				if distance < closest {
-					closest = distance
-					ant.closestFood = loc
-					ant.huntingFood = true
-				}
-			}
-		});
-	}
-	
+	movesMade := []*Ant{}
 	safeMove := func(loc Location, dir Direction) bool {
 		target := s.Map.Move(loc, dir)
 		if (s.Map.SafeDestination(target)) {
+			me.ants[loc].target = target;
+			movesMade = append(movesMade, me.ants[loc])
+
 			s.IssueOrderLoc(loc, dir)
 			return true
 		}
 		return false
 	}
+	
+	// Anything our ants can currently see doesn't need to be explored, mark it as such
+	for _, ant := range me.ants {
+		s.Map.DoInRad(ant.loc, s.ViewRadius2, func(row, col int) {
+			loc := s.Map.FromRowCol(row, col)
+			me.visible[loc] = 1.0
+		});
+	}
+	
+	// Idle or exploring ants will hunt for food if they find any
+	for _, ant := range me.ants {
+		if (ant.state == STATE_IDLE || ant.state == STATE_EXPLORE) {
+			closest := 999999999
+			fRow, fCol := s.Map.FromLocation(ant.loc)
+			s.Map.DoInRad(ant.loc, s.ViewRadius2, func(row, col int) {
+				loc := s.Map.FromRowCol(row, col)
+				if s.Map.Food[loc] {
+					distance := (fRow-row)*(fRow-row)+(fCol-col)*(fCol-col);
+					if distance < closest {
+						closest = distance
+						ant.closestFood = loc
+						ant.state = STATE_HUNT_FOOD
+					}
+				}
+			});
+		}
+	}	
 
-	// Priorities:
-	// 1. Get food
-	// 2. Explore
-	for _, ant := range myAnts {
-		if ant.huntingFood {
+	// Hunting for food now, as we may have switched other ants into this state
+	for _, ant := range me.ants {
+		if (ant.state == STATE_HUNT_FOOD) {
 			// Move towards the food
 			finalState := func(current Location) bool {
 				return current == ant.closestFood
@@ -126,8 +169,14 @@ func (me *GarboAnt) DoTurn(s *State) os.Error {
 			targetDir, valid := me.SearchMap(s, ant.loc, finalState)
 			if (valid) {
 				safeMove(ant.loc, targetDir)
-			}
-		} else {
+			} else {
+				ant.state = STATE_EXPLORE;
+			}			
+		}
+	}
+	
+	for _, ant := range me.ants {
+		if (ant.state == STATE_EXPLORE) {
 			// Explore
 			finalState := func(current Location) bool {
 				return me.visible[current] != 1.0
@@ -137,6 +186,13 @@ func (me *GarboAnt) DoTurn(s *State) os.Error {
 				safeMove(ant.loc, targetDir)
 			}
 		}
+	}
+	
+	// Go through all the moves, and update the ant states
+	for _, ant := range movesMade {
+		me.ants[ant.target] = me.ants[ant.loc]
+		me.ants[ant.loc] = nil, false
+		ant.loc = ant.target;
 	}
 	
 	log.Println("Finished turn in ms")
